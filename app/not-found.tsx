ae8ebdc7df9a330ec12cd59015b3756068218a05
aftabrehan <aftabrehan52422@gmail.com>
2024-02-02 19:16:29 +0500

    Create 404 Page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,27 @@
-const NotFoundPage = () => (
-  <div className="w-full h-full">
-    <p>404 Page</p>
-  </div>
-)
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { Button } from '@/components/button'
+
+import NotFoundImage from '@/public/404.svg'
+
+const NotFoundPage = () => {
+  const router = useRouter()
+
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-6 p-10 bg-white dark:bg-midnight-slate rounded-3xl">
+      <NotFoundImage className="w-full max-w-[342px] h-auto" />
+      <p className="text-black dark:text-white text-lg font-medium">
+        Page Not Found!
+      </p>
+      <Button
+        label="Go to Home"
+        variant="primary"
+        onClick={() => router.push('/')}
+      />
+    </div>
+  )
+}
 
 export default NotFoundPage
